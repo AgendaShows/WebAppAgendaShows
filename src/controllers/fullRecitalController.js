412f96c597dbf4c3a8929f19726d381f6165b203
Mauricio <maurisan4011@gmail.com>
2020-11-16 20:56:08 -0300

    se modifica  Servicio ObternerFullRecital Version estable , Matcheo y  consulta de consultas  en  base a datos de Recital , Validaciones .

--- src/controllers/fullRecitalController.js
+++ src/controllers/fullRecitalController.js
@@ -1,13 +1,46 @@
 const recitalesService = require('../services/recitalesService');
 const locacionesService = require('../services/locacionesService');
-
-const bandasService = require('../services/bandasService')
+const bandasService = require('../services/bandasService');
 
 
 const getFullRecitales = async (req, res) => {
-    const recital = await recitalesService.getRecitalById(req.params.id)
-    const banda = await bandasService.getBandaByNombre({ "nombre": "Metallica" })
-    const locacion = await locacionesService.getLocacionByNombre({ "nombre": "Luna Park" })
+
+    let recital;
+    let banda;
+    let locacion;
+
+    //! Capturo error  y corto el flujo  del circuito cuando servicio esta abajo de la collection  Recitales
+    try {
+
+        //* Matchea y realiza la consulta por id Recital 
+        recital = await recitalesService.getRecitalById(req.params.id)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+
+
+    //todo :Capturo error  y corto el flujo  del circuito cuando servicio esta abajo de la collection  Bandas
+    try {
+        //* Matchea y realiza la consulta por nombre 
+        banda = await bandasService.getBandaByNombre({ "nombre": recital.nombreBanda })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+
+
+    //? Capturo error  y corto el flujo  del circuito cuando servicio esta abajo de la collection Locaciones
+    try {
+        //* Matchea y realiza la consulta por nombre
+        locacion = await locacionesService.getLocacionByNombre({ "nombre": recital.ubicacion })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
 
     let fullRecital = {
         recital,
@@ -15,7 +48,10 @@ const getFullRecitales = async (req, res) => {
         locacion
     }
 
+    //! Capturo error  y corto el flujo del circuito si el Response esta roto 
     try {
+        //* REspuesta completa del Json
+        //* Encaso alguna collection no contenga datos devolvera los objetos de la collection vacios
         res.json(fullRecital)
     } catch (error) {
         res.status(500).json({
